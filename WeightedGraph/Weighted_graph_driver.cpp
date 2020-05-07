82d0510f480c3d3a073133687eca1ec315910e41
Meg Alapati <abmfocus4@gmail.com>
2020-05-06 22:51:30 -0400

    increase current capacity

---
+++ WeightedGraph/Weighted_graph_driver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Weighted_graph_tester.h"
+
+int main() {
+	Weighted_graph_tester tester;
+
+	std::cout << "Starting Test Run" << std::endl;
+
+	tester.run();
+
+	std::cout << "Finishing Test Run" << std::endl;
+
+	return 0;
+}
